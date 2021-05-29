38632295f64d5acaeb26d6d51c1949fec60e124f
a-kalayda <a.kalaida@gmail.com>
2021-05-29 14:48:30 +0300

    Init commit

---
+++ src/main/java/ru/kalayda/spring/course/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package ru.kalayda.spring.course;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.scan("ru.kalayda.spring.course");
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("mvc", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+    }
+}
